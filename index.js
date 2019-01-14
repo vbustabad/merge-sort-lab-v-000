56fe8c919ee393fbefa03fda3513dba33912a1b4
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 21:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array){
+  return array.shift();
+}
 
+function merge(firstSubarray, secondSubArray){
+  let sorted = [];
+
+  while (firstSubarray.length != 0 && secondSubArray.length != 0) {
+    let firstMin = findMinAndRemoveSorted(firstSubarray);
+    let secondMin = findMinAndRemoveSorted(secondSubArray);
+    sorted.push(firstMin);
+    sorted.push(secondMin);
+  }
+  return sorted;
+}
+
+function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+
+    if(array.length < 2){
+      return array
+    } else {
+      merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+  }
